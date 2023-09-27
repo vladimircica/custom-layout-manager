7eef3e8cbe581d86e9e427dd5b7562b0c8625483
v.cica <v.cica@levi9.com>
2023-09-27 20:50:18 +0200

    Added logic to revert matrices and elements to support RTL layout

--- app/src/main/java/com/synchrotek/customlayoutmanager/CustomLayoutManager.kt
+++ app/src/main/java/com/synchrotek/customlayoutmanager/CustomLayoutManager.kt
@@ -12,6 +12,13 @@ class CustomLayoutManager(
 ) : RecyclerView.LayoutManager() {
 
     private var horizontalOffSet: Int = 0
+
+    //TODO This should be added to constructor to be able to configure manually
+    //TODO Use this value for reverse layout
+    private var reverseLayout: Boolean = false
+
+    // TODO Change this not to be hardcoded values
+    // TODO For width and height
     private val viewWidth = 300
     private val viewHeight = 95
 
@@ -19,6 +26,7 @@ class CustomLayoutManager(
         RecyclerView.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
 
     override fun onLayoutChildren(recycler: RecyclerView.Recycler, state: RecyclerView.State) {
+        println("onLayoutChildren")
         fill(recycler)
     }
 
@@ -61,9 +69,14 @@ class CustomLayoutManager(
         return max(0, totalMatrixWidth * totalMatrices - width)
     }
 
-    private fun fill(recycler: RecyclerView.Recycler) {
+    //TODO This method should be refactored and used for both LTR and RTL
+    //TODO For now use just fillTemp method. Will merge it with fill
+    private fun fillTemp(recycler: RecyclerView.Recycler) {
         detachAndScrapAttachedViews(recycler)
         val itemsPerMatrix = rows * columns
+        val matrices = itemCount / itemsPerMatrix
+        val totalContentWidth = columns * itemsPerMatrix
+
         for (i in 0 until itemCount) {
             val matrixIndex = i / itemsPerMatrix
             val itemIndexInMatrix = i % itemsPerMatrix
@@ -76,17 +89,69 @@ class CustomLayoutManager(
             val top = row * viewHeight
             val bottom = top + viewHeight
 
+
             val view = recycler.getViewForPosition(i)
             addView(view)
 
             measureChild(view, viewWidth, viewHeight)
 
-            layoutDecorated(view, left, top, right, bottom)
+            layoutDecoratedWithMargins(view, left, top, right, bottom)
         }
 
         val scrapListCopy = recycler.scrapList.toList()
         scrapListCopy.forEach {
             recycler.recycleView(it.itemView)
         }
+    }
+
+    private fun fill(recycler: RecyclerView.Recycler) {
+        detachAndScrapAttachedViews(recycler)
+        val itemsPerMatrix = rows * columns
+        val matrices = itemCount / itemsPerMatrix
+        println("matrices number -> $matrices")
+
+        val totalContentWidth = columns * itemsPerMatrix
+
+        for (i in 0 until itemCount) {
+            val matrixIndex = (i / itemsPerMatrix)
+            val itemIndexInMatrix = (itemsPerMatrix - 1) - (i % itemsPerMatrix)
+            val col = itemIndexInMatrix % columns
+            val row = (itemsPerMatrix / columns - 1) - (itemIndexInMatrix / columns)
+
+            val left =
+                totalContentWidth - (((columns - 1 - col) + matrixIndex * columns) * viewWidth - horizontalOffSet)
+            val right = left + viewWidth
+            val top = row * viewHeight
+            val bottom = top + viewHeight
+
+
+            val view = recycler.getViewForPosition(i)
+            addView(view)
+
+            measureChild(view, viewWidth, viewHeight)
+
+            layoutDecoratedWithMargins(view, left, top, right, bottom)
+        }
+
+        val scrapListCopy = recycler.scrapList.toList()
+        scrapListCopy.forEach {
+            recycler.recycleView(it.itemView)
+        }
+    }
+
+    private fun calculateTotalContentWidth(): Int {
+        // Calculate the total width of your content here
+        // You may need to iterate through your items and matrices
+        // and sum up the widths of all elements
+        var totalWidth = 0
+        for (i in 0 until itemCount) {
+            val childView = getChildAt(i)
+            if (childView != null) {
+                val childWidth = getRightDecorationWidth(childView)
+                totalWidth += childWidth
+            }
+        }
+
+        return totalWidth
     }
 }
